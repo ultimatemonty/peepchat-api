0d5e30130e8853957df78bf99bcc2c70ae01d024
Chris McCuller <cmcculler@salesforce.com>
2016-08-29 15:38:30 -0400

    Install and configure ComeOnIn

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Peepchat.Mixfile do
   def application do
     [mod: {Peepchat, []},
      applications: [:phoenix, :phoenix_pubsub, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,8 @@ defmodule Peepchat.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:cors_plug, "~> 1.1"},
-     {:guardian,"~> 0.12.0"}]
+     {:guardian,"~> 0.12.0"},
+     {:comeonin, "~> 2.4"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
