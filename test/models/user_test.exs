b26a11fb3f08dc757268e5ffc40507b7c0660eb9
Chris McCuller <cmcculler@salesforce.com>
2016-08-29 16:16:30 -0400

    Update model requirements. Add some validation and tests

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -3,8 +3,30 @@ defmodule Peepchat.UserTest do
 
   alias Peepchat.User
 
-  @valid_attrs %{email: "some content", password_hash: "some content"}
+  @valid_attrs %{email: "peep@chat.app",
+    password: "abcde12345",
+    password_confirmation: "abcde12345"}
   @invalid_attrs %{}
+
+  test "mis-matched password_confirmation does not work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "asd32131r2qs",
+      password_confirmation: "32q321321weda"})
+    refute changeset.valid?
+  end
+
+  test "missing password_confirmation does not work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "123qr3213we"})
+    refute changeset.valid?
+  end
+
+  test "short password does not work" do
+    changeset = User.changeset(%User{}, %{email: "joe@example.com",
+      password: "12456",
+      password_confirmation: "123456"})
+    refute changeset.valid?
+  end
 
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
