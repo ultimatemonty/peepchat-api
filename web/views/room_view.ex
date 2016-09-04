77c08429ee6b2934cea8caa484c2c660f25a0e98
Chris McCuller <cmcculler@salesforce.com>
2016-09-04 16:19:27 -0400

    Begin adding room and user controllers in prep for channels

---
+++ web/views/room_view.ex
@@ -0,0 +1,25 @@
+defmodule Peepchat.RoomView do
+  use Peepchat.Web, :view
+  use JaSerializer.PhoenixView
+
+  attributes: [:name]
+  has_one :owner,link: :user_link
+
+  def user_link(room, conn) do
+    user_url(conn, :show, room.owner_id)
+  end
+
+  def render("index.json", %{rooms: rooms}) do
+    %{data: render_many(rooms, Peepchat.RoomView, "room.json")}
+  end
+
+  def render("show.json", %{room: room}) do
+    %{data: render_one(room, Peepchat.RoomView, "room.json")}
+  end
+
+  def render("room.json", %{room: room}) do
+    %{id: room.id,
+      name: room.name,
+      owner_id: room.owner_id}
+  end
+end
--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -1,5 +1,13 @@
 defmodule Peepchat.UserView do
   use Peepchat.Web, :view
+  use JaSerializer.PhoenixView
+
+  attributes: [:email]
+  has_many: :rooms, link: :rooms_link
+
+  def rooms_link(user, conn) do
+    user_rooms_url(conn, :index, user.id)
+  end
 
   def render("index.json", %{users: users}) do
       %{data: render_many(users, Peepchat.UserView, "user.json")}
